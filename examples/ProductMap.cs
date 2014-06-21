047c4452fd9d03837ba86830eda1ad17d426a5d2
Henk Mollema <henkmollema@gmail.com>
2014-06-21 16:33:31 +0200

    Update ProductMap.cs

--- examples/ProductMap.cs
+++ examples/ProductMap.cs
@@ -2,9 +2,9 @@ using Dapper.FluentMap.Mapping;
 
 namespace App.Data.Mapping
 {
-    /// <summary>
-    /// Represents a manual mapping for the Product entity.
-    /// </summary>
+	/// <summary>
+	/// Represents a manual mapping for the Product entity.
+	/// </summary>
 	public class ProductMap : EntityMap<Product>
 	{
 		public ProductMap()
@@ -12,11 +12,11 @@ namespace App.Data.Mapping
 			// Map property 'Name' to column 'strName'.
 			Map(p => p.Name)
 				.ToColumn("strName");
-
+	
 			// Map property 'Description' to 'strdescription', ignoring casing.
 			Map(p => p.Description)
 				.ToColumn("strdescription", caseSensitive: false);
-
+	
 			// Ignore the 'LastModified' property when mapping.
 			Map(p => p.LastModified)
 				.Ignore();
