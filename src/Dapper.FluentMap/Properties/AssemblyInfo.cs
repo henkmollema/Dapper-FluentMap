ddc849f5546af58dd03b77702606468f96fd4d1b
henkmollema <henkmollema@gmail.com>
2014-07-11 12:48:08 +0200

    Deploy v1.3.1.

--- src/Dapper.FluentMap/Properties/AssemblyInfo.cs
+++ src/Dapper.FluentMap/Properties/AssemblyInfo.cs
@@ -22,14 +22,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4bec5ee4-bd0e-4760-badb-5f35d076133f")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyVersion("1.3.1")]
