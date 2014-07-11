d797bb707205a60b8f82cb095003bcc468cd82ba
henkmollema <henkmollema@gmail.com>
2014-07-11 12:53:38 +0200

    Deploy Dapper.FluentMap.Dommel v1.0.3.

--- src/Dapper.FluentMap.Dommel/Properties/AssemblyInfo.cs
+++ src/Dapper.FluentMap.Dommel/Properties/AssemblyInfo.cs
@@ -22,15 +22,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ef9b2624-ef04-4231-8f5a-55c41929b4a1")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
